569fd7b6aae472f7a63e4284511c512a5920762e
eanucc <45760389+eanucc@users.noreply.github.com>
2020-12-27 09:30:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash' feeds.conf.default
+sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
+
 
